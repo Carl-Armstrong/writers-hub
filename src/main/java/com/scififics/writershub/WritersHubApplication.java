738839a863428b7c219eaaca159a9e9efe201066
Carl <carljarms@gmail.com>
2020-04-17 02:00:01 -0500

    Initial commit

---
+++ src/main/java/com/scififics/writershub/WritersHubApplication.java
@@ -0,0 +1,13 @@
+package com.scififics.writershub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WritersHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WritersHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scififics/writershub/WritersHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scififics.writershub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WritersHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
