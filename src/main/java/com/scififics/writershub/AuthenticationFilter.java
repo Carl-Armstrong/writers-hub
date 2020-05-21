07eec80dea01f6ade376b62400931e92a3a4dbc8
Carl <carljarms@gmail.com>
2020-05-21 16:39:59 -0500

    Authentication filter added /w whitelist

---
+++ src/main/java/com/scififics/writershub/AuthenticationFilter.java
@@ -0,0 +1,62 @@
+package com.scififics.writershub;
+
+import com.scififics.writershub.controllers.AuthenticationController;
+import com.scififics.writershub.data.UserRepository;
+import com.scififics.writershub.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthenticationFilter extends HandlerInterceptorAdapter {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    private static final List<String> whitelist = Arrays.asList("/login", "/register", "/logout", "/css", "/home");
+
+    private static boolean isWhitelisted(String path) {
+        if (path.equals("/")) {
+            return true;
+        }
+
+        for (String pathRoot : whitelist) {
+            if (path.startsWith(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws IOException {
+
+        // Don't require sign-in for whitelisted pages
+        if (isWhitelisted(request.getRequestURI())) {
+            // returning true indicates that the request may proceed
+            return true;
+        }
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        // The user is logged in
+        if (user != null) {
+            return true;
+        }
+
+        // The user is NOT logged in
+        response.sendRedirect("/login");
+        return false;
+    }
+}
