a45a8dd7f9f756a1b5c73d148868791b71c89729
Carl <carljarms@gmail.com>
2020-05-26 14:59:17 -0500

    Added Tags to view

--- src/main/java/com/scififics/writershub/controllers/PostcraftController.java
+++ src/main/java/com/scififics/writershub/controllers/PostcraftController.java
@@ -6,7 +6,6 @@ import com.scififics.writershub.data.TagRepository;
 import com.scififics.writershub.models.Chapter;
 import com.scififics.writershub.models.Story;
 import com.scififics.writershub.models.Tag;
-import com.scififics.writershub.models.dto.ChapterTagDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +13,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -33,17 +33,21 @@ public class PostcraftController {
     public String renderCreateChapterForm(Model model) {
         model.addAttribute("stories", storyRepository.findAll());
         model.addAttribute(new Chapter());
+        model.addAttribute("tags", tagRepository.findAll());
         return "postcraft/post";
     }
 
     @PostMapping("post")
-    public String processCreateChapterForm(@ModelAttribute @Valid Chapter newChapter,  Errors errors, Model model,
-                                           @RequestParam int storyId) {
+    public String processCreateChapterForm(@ModelAttribute @Valid Chapter newChapter, Errors errors, Model model,
+                                           @RequestParam int storyId, @RequestParam List<Integer> tags) {
 
         if (errors.hasErrors()) {
             model.addAttribute("stories", storyRepository.findAll());
+            model.addAttribute("tags", tagRepository.findAll());
             return "postcraft/post";
         }
+
+        List<Tag> tagObjs = (List<Tag>) tagRepository.findAllById(tags);
 
         Optional<Story> storyResult = storyRepository.findById(storyId);
         if (storyResult.isEmpty()) {
@@ -53,37 +57,12 @@ public class PostcraftController {
             newChapter.setStory(story);
         }
 
+        for (Tag tag : tagObjs) {
+            newChapter.addTag(tag);
+        }
+
         chapterRepository.save(newChapter);
         return "redirect:../";
     }
 
-    @GetMapping("add-tag")
-    public String displayAddTagForm(@RequestParam Integer chapterId, Model model){
-        Optional<Chapter> result = chapterRepository.findById(chapterId);
-        Chapter chapter = result.get();
-        model.addAttribute("tags", tagRepository.findAll());
-        ChapterTagDTO chapterTag = new ChapterTagDTO();
-        chapterTag.setChapter(chapter);
-        model.addAttribute("chapterTag", chapterTag);
-
-        return "postcraft/add-tag";
-    }
-
-    @PostMapping("add-tag")
-    public String processAddTagForm(@ModelAttribute @Valid ChapterTagDTO chapterTag,
-                                    Errors errors, Model model) {
-
-        if (!errors.hasErrors()) {
-            Chapter chapter = chapterTag.getChapter();
-            Tag tag = chapterTag.getTag();
-            if (chapter.getTags().contains(tag)){
-                chapter.addTag(tag);
-                chapterRepository.save(chapter);
-            }
-
-        }
-
-        return "redirect:/add-tag";
-
-    }
 }
