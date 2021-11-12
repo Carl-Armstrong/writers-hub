cb19580b7e815d58a07e6bad472fb849dc2eaf30
Carl Armstrong <carljarms@gmail.com>
2021-11-11 18:24:10 -0600

    Dropped in c# project

---
+++ Models/AbstractContentEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace writers_hub.Models
+{
+    public class AbstractContentEntity
+    {
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace writers_hub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
